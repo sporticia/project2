6bb462d15d5c90b96999b07d0b44edf19f98366c
Scott Brewerton <scott.brewerton@and.digital>
2023-02-27 15:21:43 +0000

    Use doc naming of env var

--- scripts/buildAndPushContainerImage.ps1
+++ scripts/buildAndPushContainerImage.ps1
@@ -1,2 +1,2 @@
 Write-Host "This is a script that could use $env:SYSTEM_ACCESSTOKEN"
-Write-Host "$env:SYSTEM_ACCESSTOKEN = $(System.AccessToken)"
+Write-Host "$env:SYSTEM_ACCESSTOKEN = ${System.AccessToken}"
