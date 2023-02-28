ed845984a3e6306c5a775f40b1b6311c9562eff9
Scott Brewerton <scott.brewerton@and.digital>
2023-02-28 17:05:07 +0000

    Update var name

--- scripts/buildAndPushContainerImage.ps1
+++ scripts/buildAndPushContainerImage.ps1
@@ -2,8 +2,3 @@ Write-Host "This is a script that could use $env:SYSTEM_ACCESSTOKEN"
 Write-Host "$env:SYSTEM_ACCESSTOKEN = ${System.AccessToken}"
 
 $dockerfilePath = "../pipelines/ui/Dockerfile"
-
-docker build `
-  -f $dockerfilePath `
-  --build-arg AZURE_DEVOPS_EXT_PAT=${AZURE_DEVOPS_EXT_PAT} `
-  --target final
