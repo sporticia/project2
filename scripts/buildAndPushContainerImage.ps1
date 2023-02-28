a3b7801d44ba0db97cd8d5d83c4156a5c5ea4408
Scott Brewerton <scott.brewerton@and.digital>
2023-02-28 17:08:30 +0000

    Update

--- scripts/buildAndPushContainerImage.ps1
+++ scripts/buildAndPushContainerImage.ps1
@@ -2,3 +2,5 @@ Write-Host "This is a script that could use $env:AZURE_DEVOPS_EXT_PAT"
 Write-Host "$env:AZURE_DEVOPS_EXT_PAT = ${System.AccessToken}"
 
 $dockerfilePath = "../pipelines/ui/Dockerfile"
+
+cat docker/secrets/npm_token.txt
